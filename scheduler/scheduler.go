cfdc0a298fb21bfb8dcbc24d35fbd66812f25f48
Suslov_a <suslov_a@ddc.by>
2025-06-02 13:42:49 +0300

    some changes in scheduler

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -12,23 +12,32 @@ import (
 
 	"github.com/go-co-op/gocron"
 	"gopkg.in/telebot.v4"
-) //todo make notification in week before, not in monday
+)
 
 func StartScheduler(bot *telebot.Bot, userService sv.UserService) {
 	location, _ := time.LoadLocation(constants.LOCATION)
 	s := gocron.NewScheduler(location)
+
 	scheduleTime := os.Getenv("NOTIFICATION_TIME")
 	if scheduleTime == "" {
 		scheduleTime = "10:00"
 	}
+
+	scheduleTimeWeekly := os.Getenv("NOTIFICATION_TIME_WEEKLY")
+	if scheduleTimeWeekly == "" {
+		scheduleTimeWeekly = "9:55"
+	}
+
 	_, err := s.Every(1).Days().At(scheduleTime).Do(sendDailyNotifications, bot, userService)
 	if err != nil {
 		log.Fatalf("Error scheduling task: %v", err)
 	}
-	_, err = s.Every(1).Monday().At(scheduleTime).Do(sendWeeklyNotifications, bot, userService)
+
+	_, err = s.Every(1).Monday().At(scheduleTimeWeekly).Do(sendWeeklyNotifications, bot, userService)
 	if err != nil {
 		log.Fatalf("Error scheduling task: %v", err)
 	}
+
 	s.StartAsync()
 }
 
@@ -41,11 +50,13 @@ func sendDailyNotifications(bot *telebot.Bot, userService sv.UserService) {
 			log.Printf("Error sending daily notifications: %v", sendErr)
 		}
 	}
+
 	birthdayTomorrow, others := splitUsersByBirthday(users, 1)
 	response := makeResponse(birthdayTomorrow)
+
 	if len(birthdayTomorrow) > 0 {
 		for _, other := range others {
-			_, err = bot.Send(telebot.ChatID(other.ID), response, telebot.ModeMarkdown)
+			_, err := bot.Send(telebot.ChatID(other.ID), response)
 			if err != nil {
 				log.Printf("Failed to send to user %d: %v", other.ID, err)
 			}
@@ -62,11 +73,13 @@ func sendWeeklyNotifications(bot *telebot.Bot, userService sv.UserService) {
 			log.Printf("Error sending daily notifications: %v", sendErr)
 		}
 	}
+
 	birthdayInWeek, others := splitWeeklyUsersByBirthday(users)
 	response := makeWeeklyResponse(birthdayInWeek)
+
 	if len(birthdayInWeek) > 0 {
 		for _, other := range others {
-			_, err = bot.Send(telebot.ChatID(other.ID), response, telebot.ModeMarkdown)
+			_, err := bot.Send(telebot.ChatID(other.ID), response)
 			if err != nil {
 				log.Printf("Failed to send to user %d: %v", other.ID, err)
 			}
@@ -96,7 +109,8 @@ func splitUsersByBirthday(users []sv.UserDto, daysBefore int) (birthdayTomorrow 
 
 func makeResponse(users []sv.UserDto) string {
 	var response strings.Builder
-	response.WriteString("Доброе утро!\nЗавтра день рождения у:\n")
+	now := time.Now().AddDate(0, 0, 1)
+	response.WriteString(fmt.Sprintf("Доброе утро!\nЗавтра (%s)  день рождения у:\n\n", now.Format("02.01.2006")))
 	for _, user := range users {
 		response.WriteString(fmt.Sprintf("(%s) %s %s\n\n", user.Username, user.Surname, user.Name))
 	}
@@ -111,7 +125,11 @@ func splitWeeklyUsersByBirthday(users []sv.UserDto) (birthdayInWeek []sv.UserDto
 		bd := user.Birthdate
 		currentYearBD := time.Date(today.Year(), bd.Month(), bd.Day(), 0, 0, 0, 0, time.UTC)
 
-		if (currentYearBD.After(today) || currentYearBD.Equal(today)) && currentYearBD.Before(weekLater) {
+		if currentYearBD.Before(today) {
+			currentYearBD = currentYearBD.AddDate(1, 0, 0)
+		}
+
+		if !currentYearBD.After(weekLater) {
 			birthdayInWeek = append(birthdayInWeek, user)
 		} else {
 			others = append(others, user)
@@ -123,9 +141,9 @@ func splitWeeklyUsersByBirthday(users []sv.UserDto) (birthdayInWeek []sv.UserDto
 
 func makeWeeklyResponse(users []sv.UserDto) string {
 	var response strings.Builder
-	response.WriteString("Доброе утро!\nНа этой неделе день рождения у:\n")
+	response.WriteString("Доброе утро!\nЧерез неделю будет день рождения у:\n\n")
 	for _, user := range users {
-		response.WriteString(fmt.Sprintf("(%s) %s %s. День рождения: %s\n\n", user.Username, user.Surname, user.Name, user.Birthdate.Format("02.01.2006")))
+		response.WriteString(fmt.Sprintf("(%s) %s %s.\nДень рождения: %s\n\n", user.Username, user.Surname, user.Name, user.Birthdate.Format("02.01.2006")))
 	}
 	return response.String()
 }
